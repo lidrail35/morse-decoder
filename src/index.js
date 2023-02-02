05a85abeab74872ffab3d1e45257882d0f17c4f1
lidrail35 <lidrail35@mail.ru>
2023-02-02 11:43:13 +0300

    feat: add solve tast

--- src/index.js
+++ src/index.js
@@ -38,7 +38,22 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    // write your solution here 
+    let str='';
+    let arrLetter = [];
+    let arrWord = expr.split('**********');
+    for (let i=0; i < arrWord.length; i++) {
+        arrLetter[i] = arrWord[i].match(/\w{1,10}/gi).map(x=>x.replace(/00/g, '')).map(x=>x.replace(/10/g, '.')).map(x=>x.replace(/11/g, '-'));
+    }
+
+    for (let i=0; i < arrLetter.length; i++) {
+      for (let j=0; j < arrLetter[i].length; j++) {
+         str = str + MORSE_TABLE[arrLetter[i][j]];
+      }
+      str = str+' ';
+    }
+    /*console.log(str);*/
+    return str.slice(0, -1);
 }
 
 module.exports = {
